8bf240a1f360d06f77d8e227c19e36a7a1f68d33
jayden2 <jaydenspencer1@gmail.com>
2015-09-23 14:54:11 +0800

    working on services and campus data

--- src/js/services/dashboardService.js
+++ src/js/services/dashboardService.js
@@ -1,5 +1,5 @@
 app.factory('dashboardService', ['$http', function($http) {
-	return $http.get('https://github.com/jayden2/WayFinder/blob/master/src/js/services/sample_data/campusData.json')
+	return $http.get('sample_data/campusData.json')
 	.success(function(data) {
 		return data;
 	})

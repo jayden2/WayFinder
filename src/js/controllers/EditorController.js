d3afd14afe712b4bed135a38cbd2005245acea5b
Jayden Spencer <jaydenspencer1@gmail.com>
2015-10-08 19:59:56 +0800

    added search filter and worked on toggle filter

--- src/js/controllers/EditorController.js
+++ src/js/controllers/EditorController.js
@@ -1,14 +1,33 @@
-app.controller('EditorController',['$scope', '$filter', 'mapService', function($scope, $filter, mapService) {
+app.controller('EditorController',['$scope', 'mapService', function($scope, mapService) {
 	mapService.success(function(data) {
 		$scope.maps = data;
-		console.log($scope.maps);
 	});
 	$scope.filters = { };
+	$scope.predicate = 'map.name';
+	$scope.reverse = false;
+	$scope.order = function(predicate) {
+		console.log(predicate);
+		$scope.filters = { };
+		if (predicate === 'num0') {
+			//unsure
+		}
+		if (predicate === 'num9') {
+			//unsure
+		}
+		if (predicate === 'nameA') {
+			$scope.reverse = false;
+			$scope.predicate = 'map.name';
+		}
+		if (predicate === 'nameZ') {
+			$scope.reverse = true;
+			$scope.predicate = 'map.name';
+		}
+	}
 	$scope.selectionFilter = function(query) {
 		$scope.filters = { };
-		if (query == 'unlink') {
+		if (query === 'unlink') {
 			$scope.filters.link = false;
-		} else if (query == 'favourite') {
+		} else if (query === 'favourite') {
 			$scope.filters.favourite = true;
 		} else {
 			$scope.filters = { };

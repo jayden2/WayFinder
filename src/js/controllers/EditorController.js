7f5cd9c5c4997f8c9b395e64e9533b675b1f6fda
Jayden Spencer <jaydenspencer1@gmail.com>
2015-10-08 18:41:51 +0800

    added filter unlinked and favourited

--- src/js/controllers/EditorController.js
+++ src/js/controllers/EditorController.js
@@ -1,8 +1,19 @@
-app.controller('EditorController',['$scope', 'mapService', function($scope, mapService) {
+app.controller('EditorController',['$scope', '$filter', 'mapService', function($scope, $filter, mapService) {
 	mapService.success(function(data) {
 		$scope.maps = data;
 		console.log($scope.maps);
 	});
+	$scope.filters = { };
+	$scope.selectionFilter = function(query) {
+		$scope.filters = { };
+		if (query == 'unlink') {
+			$scope.filters.link = false;
+		} else if (query == 'favourite') {
+			$scope.filters.favourite = true;
+		} else {
+			$scope.filters = { };
+		}
+	}
 	$scope.favouriteState = function() {
     };
 }]);
